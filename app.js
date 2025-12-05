272dae17195d47a5c390212b431550684ba79c67
Luca Deeken <luca.deeken@fuchs-edv.de>
2025-12-05 11:05:41 +0100

    feat: build basal structure of the nodeProject.

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const path = require("node:path");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+//Import Routes
+const indexRouter = require("./routes/index");
+app.use("/", indexRouter);
+
+const PORT = 3000;
+app.listen(PORT, (error) => {
+  if (error) {
+    throw error;
+  }
+  console.log(`Listening on port ${PORT}`);
+});
